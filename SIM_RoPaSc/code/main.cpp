8d64704aa160b9758ba6edcf135e444e331f3626
Jalaj Dixit <jalajdixit1@gmail.com>
2024-04-11 01:11:47 +0200

    Falling stone animation

--- SIM_RoPaSc/code/main.cpp
+++ SIM_RoPaSc/code/main.cpp
@@ -8,6 +8,8 @@ using sf::RenderWindow;
 using sf::VideoMode;
 using sf::Event;
 
+int rock_yaxis = 50;
+
 int main()
 {
     RenderWindow window(VideoMode(600, 600), "Animation");
@@ -33,15 +35,16 @@ int main()
     sf::IntRect irectScissor(0, 0, sprite_sheet_size.x/3, sprite_sheet_size.y);
     scissor_sprite.setTextureRect(irectScissor);
 
-    rock_sprite.setPosition(sf::Vector2f(80, 50));
-    paper_sprite.setPosition(sf::Vector2f(280, 50));
-    scissor_sprite.setPosition(sf::Vector2f(480, 50));
+    rock_sprite.setPosition(sf::Vector2f(80, rock_yaxis));
+    paper_sprite.setPosition(sf::Vector2f(280, rock_yaxis));
+    scissor_sprite.setPosition(sf::Vector2f(480, rock_yaxis));
+
+    sf::Clock clock;
 
     while (window.isOpen()) {
 
         Event event;
         while (window.pollEvent(event)) {
-
             switch (event.type) {
                 case Event::Closed:
                     window.close();
@@ -50,6 +53,14 @@ int main()
                 default:
                     break;
             }
+        }
+
+        if (rock_yaxis < 560 && clock.getElapsedTime().asSeconds() > 0.01f) {
+            cout << clock.getElapsedTime().asSeconds() << endl;
+            rock_sprite.setPosition(sf::Vector2f(80, rock_yaxis++));
+            paper_sprite.setPosition(sf::Vector2f(280, rock_yaxis++));
+            scissor_sprite.setPosition(sf::Vector2f(480, rock_yaxis++));
+            clock.restart();
         }
 
         window.clear(sf::Color(104,144,168,255));
